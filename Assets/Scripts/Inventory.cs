d483ba78578c921fd6574c8db36d88144f2b958e
jamesrone <32492411+jaamesrone@users.noreply.github.com>
2023-09-14 11:44:06 -0700

    removed mechanic

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,7 +7,7 @@ public class Inventory : MonoBehaviour
 
     public void BuyMechanicItem(MechanicItem item)
     {
-        if (!mechanicItems.Contains(item) || item.maxUses <= 0)
+        if (!mechanicItems.Contains(item) || item.maxUses <= 1)
         {
             // The item is not in the inventory or has been used up; add it to the inventory
             mechanicItems.Add(item);
@@ -28,7 +28,7 @@ public class Inventory : MonoBehaviour
             Debug.Log("Mechanic item used. Remaining uses: " + item.maxUses);
 
             // If the item has no more uses left, remove it from the inventory.
-            if (item.maxUses <= 0)
+            if (item.maxUses <= 1)
             {
                 mechanicItems.Remove(item);
                 Debug.Log("Mechanic item removed from inventory.");
