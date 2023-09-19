eb657c3383063c9cc9482cf5ec91dc20c0d9396e
Domags99 <Djaguilar@mail.csuchico.edu>
2023-09-18 19:31:15 -0700

    SwayScript
    made a sway script that makes the icons sway back and fourth very slightly.

---
+++ Assets/Scripts/SwayMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwayMovement : MonoBehaviour
+{
+    //Sway Amount 
+    [SerializeField] private float swayAmount = 10f;
+    //Sway Speed
+    [SerializeField] private float swaySpeed = 1f;
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+
+        float horizontalSway = Mathf.Sin(Time.time * swaySpeed) * swayAmount;
+
+        // Apply the sway to the icon's position.
+        Vector3 newPosition = initialPosition + new Vector3(horizontalSway, 0f, 0f);
+        transform.localPosition = newPosition;
+    }
+}
